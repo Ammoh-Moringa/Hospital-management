ed0359bf598c7276a0b938e7e2bb596b20f738b3
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-27 11:23:13 +0300

    Create view function for registration

--- hospital/views.py
+++ hospital/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def index(request):
 
     return render(request,'index.html')
+
+
+def registration(request):
+    if request.method=="POST":
+        form=SignUpForm(request.POST) 
+        if form.is_valid():
+           form.save()
+           username = form.cleaned_data.get('username')
+           user_password = form.cleaned_data.get('password1')
+           user = authenticate(username=username, password=user_password)
+           login(request, user)
+        return redirect('login')
+    else:
+        form= SignUpForm()
+    return render(request, 'registration/registration_form.html', {"form":form}) 
