e4f4afca184f5ed73b0861b8b91c262494b8857d
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-27 13:02:39 +0300

    register models in Admin

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,7 @@
+from hospital.models import Bed, Doctor, Patient
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Bed)
