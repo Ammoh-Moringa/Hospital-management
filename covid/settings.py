e5258c564c8b2dec9fc55307bed4ee98acefffb1
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-27 11:08:50 +0300

    Create database hospital

--- covid/settings.py
+++ covid/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'covid.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hospital',   
+        'USER': 'moringa',      
+        'PASSWORD':'Ammoh',
     }
 }
 
