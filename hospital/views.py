0772e967303327496ea1a11e4046eee366b64db2
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-27 11:29:14 +0300

    Create signUp forms

--- hospital/views.py
+++ hospital/views.py
@@ -1,3 +1,4 @@
+from hospital.forms import SignUpForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
