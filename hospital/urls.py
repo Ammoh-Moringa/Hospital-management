42f0e4867573cf1251af827beee795c1d1834856
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-27 11:33:47 +0300

    url for registration view

--- hospital/urls.py
+++ hospital/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 
 
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),
+    url('register/',views.registration, name='registration'),
+    url('login/',auth_views.LoginView.as_view(), name='login'),
+    url('logout/',auth_views.LogoutView.as_view(), name='logout'),
 ]
 
