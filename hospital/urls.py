a0f9405b5502c874fa09eef9f634416abe070b3b
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-28 14:53:36 +0300

    Add path for add patient

--- hospital/urls.py
+++ hospital/urls.py
@@ -14,5 +14,7 @@ urlpatterns=[
     url('login/',auth_views.LoginView.as_view(), name='login'),
     url('logout/',auth_views.LogoutView.as_view(), name='logout'),
     path("patient/<str:pk>", views.patient, name="patient"),
+    path("add_patient/", views.add_patient, name="add_patient"),
+    path("patient_list/", views.patient_list, name="patient_list"),
 ]
 
