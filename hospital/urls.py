b531f5581205328a9c0af0514a9438ab2768b12e
Ammoh-Moringa <amos.kiprotich@student.moringaschool.com>
2021-07-28 11:50:13 +0300

    recreate view function for index page

--- hospital/urls.py
+++ hospital/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls import url
 from . import views
 from django.contrib.auth import views as auth_views
+from django.urls import path
 
 
 
 
 
 urlpatterns=[
-    url(r'^$',views.index,name='index'),
+    path("", views.index, name="index"),
     url('profile/',views.index,name='index'),
     url('register/',views.registration, name='registration'),
     url('login/',auth_views.LoginView.as_view(), name='login'),
     url('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    path("patient/<str:pk>", views.patient, name="patient"),
 ]
 
--- hospital/views.py
+++ hospital/views.py
@@ -1,3 +1,4 @@
+from hospital.models import Bed, Doctor, Patient
 from hospital.forms import SignUpForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
@@ -6,8 +7,24 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
+    patients = Patient.objects.all()
+    patient_count = patients.count()
+    patients_recovered = Patient.objects.filter(status="Recovered")
+    patients_deceased = Patient.objects.filter(status="Deceased")
+    deceased_count = patients_deceased.count()
+    recovered_count = patients_recovered.count()
+    beds = Bed.objects.all()
+    beds_available = Bed.objects.filter(occupied=False).count()
+    context = {
+        'patient_count': patient_count,
+        'recovered_count': recovered_count,
+        'beds_available': beds_available,
+        'deceased_count':deceased_count,
+        'beds':beds
+    }
+    print(patient_count)
 
-    return render(request,'index.html')
+    return render(request,'index.html', context)
 
 
 def registration(request):
@@ -29,4 +46,36 @@ def registration(request):
 def profile(request):
 
     return render(request,'index.html')
+
+
+def patient(request, pk):
+    patient = Patient.objects.get(id=pk)
+    if request.method == "POST":
+        doctor = request.POST['doctor']
+        doctor_time = request.POST['doctor_time']
+        doctor_notes = request.POST['doctor_notes']
+        mobile = request.POST['mobile']
+        mobile2 = request.POST['mobile2']
+        relativeName = request.POST['relativeName']
+        address  = request.POST['location']
+        print(doctor_time)
+        print(doctor_notes)
+        status = request.POST['status']
+        doctor = Doctor.objects.get(name=doctor)
+        print(doctor)
+        patient.phone_num = mobile
+        patient.patient_relative_contact = mobile2
+        patient.patient_relative_name = relativeName
+        patient.address = address
+        patient.doctor = doctor
+        patient.doctors_visiting_time = doctor_time
+        patient.doctors_notes = doctor_notes
+        print(patient.doctors_visiting_time)
+        print(patient.doctors_notes)
+        patient.status = status
+        patient.save()
+    context = {
+        'patient': patient
+    }
+    return render(request, 'patient.html', context)
  
